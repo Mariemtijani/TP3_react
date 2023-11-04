e8cb1f9e768e3a4f776fa937476049b97bde7f4a
mariem tijani <mariemtijani60@gmail.com>
2023-11-04 22:30:04 +0100

    todo list app

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+import TodoList from './components/TodoList';
+
+
+
+
+
+
+
+  function App() {
+
+   
+      
+    
+
+    return (
+      <div className="App all">
+         
+          <TodoList />
+        
+      </div>
+    )
+  }
+
+
+export default App;
